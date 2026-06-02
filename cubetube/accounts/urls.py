875219c4c7e241294e6a443793692b7876aafd54
Gamer <bscrinale@gmail.com>
2026-06-02 10:28:16 -0400

    added to urls and base template

---
+++ cubetube/accounts/urls.py
@@ -0,0 +1,17 @@
+from django.shortcuts import redirect
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+app_name = "accounts"
+
+urlpatterns = [
+    path("register/", views.RegisterView.as_view),
+    path("login/", auth_views.LoginView.as_view(
+        template_name="accounts/login.html",
+        redirect_autheticated_user=True,
+    ), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(
+        template_name="accounts/logout.html",
+    ), name="logout"),
+]
