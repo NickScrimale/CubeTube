d8907d889db99afaa5745a07ce78ef6e88dc442e
Gamer <bscrinale@gmail.com>
2026-06-02 09:24:59 -0400

    added register form

--- cubetube/accounts/views.py
+++ cubetube/accounts/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from typing import override
+
+from django.http import response
+from django.shortcuts import redirect, render
+from django.contrib.auth import login
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from .forms import CustomUserCreationForm
+
+class RegisterView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('accounts:login')
+    template_name = "accounts/register.html"
+
+    @override
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect("/")
+        return super().dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        login(self.request, self.object)
+        return redirect("/")
 
 # Create your views here.
